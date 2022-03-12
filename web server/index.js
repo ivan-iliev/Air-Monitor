b84b93896b83967fa6294cbb320541bc39871279
Ivan Iliev <mr.i.iliev@gmail.com>
2022-03-13 00:31:52 +0200

    chart js

--- web server/index.js
+++ web server/index.js
@@ -1,3 +1,7 @@
+let timestampArr = [];
+let valuesPM1 = [];
+let valuesPM25 = [];
+
 const firebaseConfig = {
     apiKey: "AIzaSyAIdXk3Ia28cgFMZISemJZMJk0bPlnxjfQ",
     authDomain: "airmonitor-7d236.firebaseapp.com",
@@ -30,7 +34,20 @@ function gotData(data) {
     for (var i = 0; i < keys.length; i++) {
         var k = keys[i];
         var a = dataDB[k];
-        console.log(a);
+        var valuePM1 = dataDB[k].ValuePM1;
+        var valuePM25 = dataDB[k].ValuePM25;
+        var timestamp = dataDB[k].Timestamp;
+
+        var date = new Date(timestamp * 1000);
+
+        var time = date.toDateString() + " " + date.toLocaleTimeString();
+        // console.log(a);
+        // console.log(valuePM1);
+        // console.log(valuePM25);
+
+        timestampArr.push(time);
+        valuesPM1.push(valuePM1);
+        valuesPM25.push(valuePM25);
     }
 
 }
@@ -42,3 +59,45 @@ function errData(err) {
     console.log("Err!");
     console.log(err);
 }
+
+
+console.log(valuesPM1);
+console.log(valuesPM25);
+console.log(timestampArr);
+
+
+const ctx = document.getElementById('myChart').getContext('2d');
+
+const DATA_COUNT = 7;
+const NUMBER_CFG = { count: DATA_COUNT, min: -100, max: 100 };
+
+//const labels = ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'];
+
+const data = {
+    labels: timestampArr,
+    datasets: [{
+            label: 'PM1',
+            data: valuesPM1,
+            borderColor: 'rgba(75, 192, 20, 0.2)',
+            backgroundColor: 'rgba(75, 192, 20, 1)',
+        },
+        {
+            label: 'PM2',
+            data: valuesPM25,
+            borderColor: 'rgba(75, 192, 20, 0.2)',
+            backgroundColor: 'rgba(75, 192, 20, 1)',
+        }
+    ]
+};
+
+const myChart = new Chart(ctx, {
+    type: 'bar',
+    data: data,
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
